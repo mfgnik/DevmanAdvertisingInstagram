e55c945493c6e57c257be26da88c7a5c589903d4
Mikhail Fedorov <misha17061995@yandex.ru>
2019-05-10 10:43:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Space Script
+Script to check giveaway conditions in Instagram.
+
+## Installation
+Install the packages from requirements.txt using pip:
+```bash
+pip3 install -r requirements.txt
+```
+After this step you should register at [Instagram](https://instagram.com/) to get login and password.
+Then open with text editor and add to it two lines. 
+```bash
+login='LOGIN_YOU_GET' 
+password='PASSWORD_YOU_GET'
+```
+'LOGIN_YOU_GET' and 'PASSWORD_YOU_GET' are your login and password.
+
+# Quick Start
+
+```bash
+python script.py
+```
+
+# Project Goals
+
+The code is written for educational purposes. Training course for web-developers - [dvmn.org](https://dvmn.org)
---
+++ advertising_script.py
@@ -0,0 +1,56 @@
+import instabot
+import os
+from dotenv import load_dotenv
+import re
+from argparse import ArgumentParser
+from random import choice
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--url_path',
+        help='path to the instagram photo',
+        required=True
+    )
+    parser.add_argument(
+        '--username',
+        help='username of account',
+        required=True
+    )
+    return parser.parse_args()
+
+
+def is_user_exists(bot, username):
+    return bot.get_user_id_from_username(username) is not None
+
+
+def check_comment(bot, comment, likers, followers):
+    regexp = r'(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)'
+    users = re.findall(regexp, comment['text'])
+    any_users_exists = any(is_user_exists(bot, user) for user in users)
+    user_id = comment['user_id']
+    return any_users_exists and user_id in followers and user_id in likers
+
+
+def check_comments(bot, url_path, username):
+    media_id = bot.get_media_id_from_link(url_path)
+    comments = bot.get_media_comments_all(media_id)
+    likers = set(bot.get_media_likers(media_id))
+    followers = set(bot.get_user_followers(bot.get_user_id_from_username(username)))
+    appropriate_comments = []
+    for comment in comments:
+        if check_comment(comment, likers, followers):
+            appropriate_comments.append(comment['username'])
+    return set(appropriate_comments)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    arguments = parse_args()
+    bot = instabot.Bot()
+    bot.login(username=os.getenv('login'), password=os.getenv('password'))
+    appropriate_comments = check_comments(bot, arguments.url_path, arguments.username)
+    winner = choice(appropriate_comments)
+    print(winner)
+
